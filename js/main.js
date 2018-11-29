5da08a0a7089c947e98a7472f4e3f73760e7d94c
Alex <achythlook@gmail.com>
2018-11-28 21:33:56 -0900

    Get thigns looking almost right

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-document.addEventListener('touchmove', (ev) => console.log(ev));
+import { html, render } from 'https://unpkg.com/lit-html?module'
+
+if (screenfull.enabled) {
+    document.querySelector('#metabuttons #fullscreen').addEventListener('click', () => screenfull.toggle());
+}
+// document.addEventListener('touchmove', (ev) => console.log(ev));
+
+function debug_fill_canvas() {
+    let c = document.getElementById('gamescreen');
+    let ctx = c.getContext('2d');
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, c.width, c.height);
+}
+debug_fill_canvas()
