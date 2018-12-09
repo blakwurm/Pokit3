7c430abb86fc520581f1268d242c46ed4f0e738a
Croug <officialtoxicdub@gmail.com>
2018-12-08 18:27:30 -0900

    Began working on rendering backend

---
+++ js/jewls/renderer.js
@@ -0,0 +1,21 @@
+﻿export const OPEN_GL = 1;
+export const BASIC = 0;
+
+let module = null;
+
+export function renderSprite(sprite, position, rotation, scale, scaleType) {
+
+}
+
+export function clear(color) {
+
+}
+
+export function setRenderBackend(backend) {
+    switch(backend) {
+        case OPEN_GL:
+            break;
+        case BASIC:
+            break;
+    }
+}
