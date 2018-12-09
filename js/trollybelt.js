3317277b62161b33b05189c313326bdc4e8c3f75
Alex Chythlook <achythlook@gmail.com>
2018-12-08 18:23:15 -0900

    Make trollybelt all kinds of OOP

--- js/trollybelt.js
+++ js/trollybelt.js
@@ -1,22 +1,64 @@
 export default class TrollyBelt {
     constructor() {
         this.scripts = new Map();
+        this.entities = new Map();
+        this.entitySorter = null;
     }
-    processEntities(entityArray) {
-        for (let entity of entityArray) {
-            for (let entry of entity) {
-                let script = this.scripts.get(entry[0]);
-                if (script) {
-                    script.call(null, entity, entityArray);
-                }
+    update() {
+        for (let entity of this.entities.values()) {
+            console.log(entity);
+            for (let key of entity.components.keys()) {
+                let script = this.scripts.get(key);
+                script.update(entity);
             }
         }
+    }
+    makeEntity() {
+        let entID = 'ent' + (Math.random() * 10000);
+        let trollybelt = this;
+        let newEnt = {
+            priority: 0,
+            id: entID,
+            components: new Map(),
+            enableComponent(key) {
+                console.log(this);
+                this.components.set(key, trollybelt.scripts.get(key).makebundle(newEnt));
+                trollybelt.scripts.get(key).init(this);
+            },
+            getComponent(key) {
+                this.components.get(key);
+            },
+            removeComponent(key) {
+                trollybelt.scripts.get(key).destroy(ent);
+                this.components.delete(key);
+            }
+        }
+        this.entities.set(entID, newEnt);
+        return newEnt;
+    }
+    registerScript(scriptname, scriptObj) {
+        this.scripts.set(scriptname, scriptObj);
     }
 }
 
 let tb = new TrollyBelt();
-tb.scripts.set('foo', function (entity, entityArray) { 
-    entity.set('ya', {});
-});
-let testentity = new Map([['foo', new Map()]]);
-tb.processEntities([testentity]);
+tb.registerScript('foo', {
+    init(ent) {
+        console.log("did a thing with ");
+        console.log(ent);
+    },
+    update(ent) {
+        console.log('updating with ');
+        console.log(ent);
+    },
+    destroy(ent) {
+        console.log('destroying on ');
+        console.log(ent);
+
+    },
+    makebundle: ent => new Map([['ba', 'ne']])
+})
+let ent = tb.makeEntity();
+ent.enableComponent('foo');
+tb.update();
+ent.removeComponent('foo');
