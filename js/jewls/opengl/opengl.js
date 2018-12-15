261cde58bdb0a705f9cd6b82b5adf80adcb2abf7
Croug <officialtoxicdub@gmail.com>
2018-12-15 00:23:29 -0900

    Added camera functionality

--- js/jewls/opengl/opengl.js
+++ js/jewls/opengl/opengl.js
@@ -83,27 +83,39 @@ export async function initContext(canvas) {
     return true;
 }
 
+export function createImageTexture(name, image) {
+    let tex = createTexture(image.width, image.height, image);
+    _textures.set(name, tex);
+}
 
+export function createRawTexture(name, width, height, data) {
+    let tex = createTexture(width, height, data);
+    _textures.set(name, tex);
+}
 
-export function createTexture(name, image) {
+function createTexture(width, height, data) {
     let texture = _gl.createTexture();
 
     _gl.bindTexture(_gl.TEXTURE_2D, texture);
-    _gl.texImage2D(_gl.TEXTURE_2D, 0, _gl.RGBA, _gl.RGBA, _gl.UNSIGNED_BYTE, image);
+    _gl.texImage2D(_gl.TEXTURE_2D, 0, _gl.RGBA, width, height, 0, _gl.RGBA, _gl.UNSIGNED_BYTE, data);
 
     _gl.texParameteri(_gl.TEXTURE_2D, _gl.TEXTURE_MAG_FILTER, _gl.NEAREST);
     _gl.texParameteri(_gl.TEXTURE_2D, _gl.TEXTURE_MIN_FILTER, _gl.NEAREST);
+    _gl.texParameteri(_gl.TEXTURE_2D, _gl.TEXTURE_WRAP_S, _gl.CLAMP_TO_EDGE);
+    _gl.texParameteri(_gl.TEXTURE_2D, _gl.TEXTURE_WRAP_T, _gl.CLAMP_TO_EDGE);
     _gl.bindTexture(_gl.TEXTURE_2D, null);
 
-    _textures.set(name, {
+    return {
         texture: texture,
-        width: image.width,
-        height: image.height,
-    });
+        width: width,
+        height: height,
+    };
 }
 
-export function createActor(name, texture, priority = 0) {
+
+export function createActor(name, texture, priority = 0, textureLiteral = false) {
     let tex = _textures.get(texture);
+    if (textureLiteral) tex = texture;
     let vertexPosition = _programs[0].attributes.vertexPosition;
 
     let positionBuffer = _gl.createBuffer();
@@ -164,6 +176,35 @@ export function createActor(name, texture, priority = 0) {
     });
 }
 
+export function createCamera(name, width, height, clearR, clearG, clearB, clearA) {
+    const fb = _gl.createFramebuffer();
+    _gl.bindFramebuffer(_gl.FRAMEBUFFER, fb);
+
+    let tex = createTexture(width, height, null);
+    _gl.framebufferTexture2D(_gl.FRAMEBUFFER, _gl.COLOR_ATTACHMENT0, _gl.TEXTURE_2D, tex.texture, 0);
+
+    _cameras.set(name, {
+        x: 0,
+        y: 0,
+        angle: 0,
+        width: width,
+        height: height,
+        frameBuffer: fb,
+        texture: tex,
+        clear: {
+            r: clearR,
+            g: clearG,
+            b: clearB,
+            a: clearA,
+        },
+    })
+}
+
+export function createCameraView(name, camera, priority = 0) {
+    let tex = _cameras.get(camera).texture;
+    createActor(name, tex, priority, true);
+}
+
 export function clear(r, g, b, a) {
     _gl.clearColor(r, g, b, a)
     _gl.clear(_gl.COLOR_BUFFER_BIT);
@@ -173,16 +214,25 @@ function toRad(deg) {
     return deg / 360 * 2 * Math.PI;
 }
 
-export function rotate(actor, degrees) {
+export function rotateActor(actor, degrees) {
     _actors.get(actor).angle = degrees;
 }
 
-export function translate(actor, x, y) {
+export function rotateCamera(camera, degrees) {
+    _cameras.get(camera).angle = degrees;
+}
+
+export function translateActor(actor, x, y) {
     _actors.get(actor).x_translation = x;
     _actors.get(actor).y_translation = y;
 }
 
-export function scale(actor, x, y) {
+export function translateCamera(camera, x, y) {
+    _cameras.get(camera).x = x;
+    _cameras.get(camera).y = y;
+}
+
+export function scaleActor(actor, x, y) {
     _actors.get(actor).x_scale = x;
     _actors.get(actor).y_scale = y;
 }
@@ -191,13 +241,41 @@ export function render(r, g, b, a) {
 
     let programData = _programs[0];
 
+    for (let camera of _cameras.values()) {
+        _gl.bindFramebuffer(_gl.FRAMEBUFFER, camera.frameBuffer);
+
+        _gl.viewport(0, 0, camera.width, camera.height);
+
+        clear(camera.clear.r, camera.clear.g, camera.clear.b, camera.clear.a);
+
+        _gl.useProgram(programData.program);
+
+        for (let actor of _actors.values()) {
+            _gl.bindVertexArray(actor.vertexArray);
+
+            _gl.activeTexture(_gl.TEXTURE0);
+            _gl.bindTexture(_gl.TEXTURE_2D, actor.texture);
+
+            _gl.uniform1i(programData.uniforms.image, 0);
+            _gl.uniform2f(programData.uniforms.resolution, camera.width, camera.height);
+            _gl.uniform2f(programData.uniforms.translation, actor.x_translation - camera.x, actor.y_translation - camera.y);
+            _gl.uniform2f(programData.uniforms.rotation, Math.sin(toRad(actor.angle - camera.angle)), Math.cos(toRad(actor.angle - camera.angle)));
+            _gl.uniform2f(programData.uniforms.scale, actor.x_scale, actor.y_scale);
+            _gl.uniform2f(programData.uniforms.uvModifier, 1.0, 1.0);
+            _gl.uniform2f(programData.uniforms.uvTranslator, 0.0, 0.0);
+
+            _gl.drawArrays(_gl.TRIANGLES, 0, 6);
+        }
+    }
+    _gl.bindFramebuffer(_gl.FRAMEBUFFER, null);
+
     _gl.viewport(0, 0, _gl.canvas.width, _gl.canvas.height);
 
     clear(r, g, b, a);
 
     _gl.useProgram(programData.program);
 
-    for (var actor of _actors.values()) {
+    for (let actor of _actors.values()) {
         _gl.bindVertexArray(actor.vertexArray);
 
         _gl.activeTexture(_gl.TEXTURE0);
