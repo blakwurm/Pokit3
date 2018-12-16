ab7cc1894413068b5b5cea732c0676bd63043972
Alex <achythlook@gmail.com>
2018-12-15 23:27:00 -0900

    Jack in Jewls stub

--- js/jewls.js
+++ js/jewls.js
@@ -1,1 +1,14 @@
 import { Bauble } from './baublebox.js';
+
+export default class JewlSystem extends Bauble{
+    constructor(pokitOS, canvas) {
+        super('jewls', Infinity);
+        this.canvas = canvas;
+        this.pokitOS = pokitOS;
+        /** Put your other init stuff here */
+    }
+    globalUpdate(components) {
+
+    }
+
+}
