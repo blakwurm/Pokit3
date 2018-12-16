975e8fe0488b4d0fab88fc8bfbee2a55d56033d0
Alex <achythlook@gmail.com>
2018-12-15 23:35:48 -0900

    JEWLS!

--- js/jewls.js
+++ js/jewls.js
@@ -1,6 +1,6 @@
 import { Bauble } from './baublebox.js';
 
-export default function initializeJewls(pokitOS) {
+export default function initializeJewls(pokitOS, canvas) {
     pokitOS.baublebox.initializeSystem('systemname', new Map());
 
 }
