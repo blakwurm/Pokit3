79b3ac1202cc19fa7274e292decb0c520592ee84
Croug <officialtoxicdub@gmail.com>
2018-12-17 02:01:26 -0900

    Add map placeholders

--- carts/krackedEC/gamecart.js
+++ carts/krackedEC/gamecart.js
@@ -2,41 +2,56 @@
 //import './kontra.js';
 //import { setupPC } from './playerControl.js';
 
+//http://localhost:8080/?cart=%2Fcarts%2FkrackedEC%2Fgamecart.js test url
+
 export class GameCart {
     constructor(pokitOS) {
         this.pokitOS = pokitOS;
         this.ecs = pokitOS.baublebox;
         this.assetPool = pokitOS.bellhop;
+        this.input = pokitOS.inputManager;
     }
     async preload() {
         //loadMap(this.pokitOS);
-        this.pokitOS.bellhop.loadImage('startScreen', '/carts/krackedEC/rawsprites/startscreen.png');
+        this.assetPool.loadImage('startScreen', '/carts/krackedEC/rawsprites/startscreen.png');
+        this.assetPool.loadImage('mapA', '/carts/krackedEC/rawsprites/mapA.png');
+        this.assetPool.loadImage('mapB', '/carts/krackedEC/rawsprites/mapB.png');
+        this.assetPool.loadImage('mapC', '/carts/krackedEC/rawsprites/mapC.png');
+        this.assetPool.loadImage('mapD', '/carts/krackedEC/rawsprites/mapD.png');
     }
-    async start() {
-        let startScreen = this.ecs.makeEntity({ x: 160, y: 160, z: -1, height: 80, width: 80, scaleX: 4, scaleY: 4, },
-            'jewlsTexture', {ID: 'startScreen', width: 80, height: 80, x: 0, y: 0},
-            'jewlsActor', {});
 
-        setupPC(this.pokitOS);
-        let camA = makeQuadCam(-8000, -8000); //Top Left Cam
-        let camB = makeQuadCam(8000, -8000);  //Top Right Cam
-        let camC = makeQuadCam(-8000, 8000);  //Bottom Left Cam
-        let camD = makeQuadCam(8000, 8000);   //Bottom Right Cam
-        let camViewA = makeQuadCamView(0, 0, camA);
-        let camViewB = makeQuadCamView(160, 0, camB);
-        let camViewC = makeQuadCamView(0, 160, camC);
-        let camViewD = makeQuadCamView(160, 160, camD);
+    async start() {
+        let startScreen = this.makeActor(160, 160, 'startScreen', 1, 80, 80, 4, 4);
+        let mapA = this.makeActor(-8000 + 80, -8000 + 80, 'mapA');
+        let mapB = this.makeActor(-8000 + 80, 8000 + 80, 'mapB');
+        let mapC = this.makeActor(8000 + 80, -8000 + 80, 'mapC');
+        let mapD = this.makeActor(8000 + 80, 8000 + 80, 'mapD');
+        //setupPC(this.pokitOS);
+        let camA = this.makeQuadCam(-8000, -8000); //Top Left Cam
+        let camB = this.makeQuadCam(8000, -8000);  //Top Right Cam
+        let camC = this.makeQuadCam(-8000, 8000);  //Bottom Left Cam
+        let camD = this.makeQuadCam(8000, 8000);   //Bottom Right Cam
+        let camViewA = this.makeQuadCamView(0, 0, camA);
+        let camViewB = this.makeQuadCamView(160, 0, camB);
+        let camViewC = this.makeQuadCamView(0, 160, camC);
+        let camViewD = this.makeQuadCamView(160, 160, camD);
+    }
+
+    makeActor(x, y, texture, z = 0, width, height, scaleX = 1, scaleY = 1) {
+        return this.ecs.makeEntity({ x: x, y: y, z: z, height: height, width: width, scaleX: scaleX, scaleY: scaleY, },
+            ['jewlsTexture', { ID: texture, width: width, height: height, x: 0, y: 0 }],
+            ['jewlsActor', {}]);
     }
 
     makeQuadCamView(x, y, camera) {
         return this.ecs.makeEntity({ x: x, y: y, z: 0, width: 160, height: 160 },
-            'jewlsCameraView', {cameraID: camera});
+            ['jewlsCameraView', {cameraID: camera}]);
     }
 
     makeQuadCam(x, y) {
         return this.ecs.makeEntity(
-            { x: x, y: y, width: 160, height: 160 },
-            'camera', {});
+            [{ x: x, y: y, width: 160, height: 160 }],
+            ['camera', {}]);
     }
 
     
