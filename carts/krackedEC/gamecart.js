a7879c4d3ae29449ab80f639745711524fc5ce61
Croug <officialtoxicdub@gmail.com>
2018-12-17 19:51:46 -0900

    Fix syntax error

--- carts/krackedEC/gamecart.js
+++ carts/krackedEC/gamecart.js
@@ -22,24 +22,34 @@ export class GameCart {
     }
 
     async start() {
+        console.log(Object.assign({
+            entityID: 'arb',
+            x: 0, y: 0, z: 0,
+            scale: 1, scaleX: 1, scaleY: 1,
+            rotation: 0,
+            width: 0, height: 0,
+            velocityX: 0, velocityY: 0,
+            requestDelete: false, willDelete: false
+        }, { x: 0, y: 0, z: 0, width: 160, height: 160 }));
+
         console.log('start happened');
         let startScreen = this.makeActor(160, 160, 'startScreen', 1, 80, 80, 4, 4);
         let mapA = this.makeActor(-8000 + 80, -8000 + 80, 'mapA');
-        let mapB = this.makeActor(-8000 + 80, 8000 + 80, 'mapB');
-        let mapC = this.makeActor(8000 + 80, -8000 + 80, 'mapC');
+        let mapB = this.makeActor(8000 + 80, -8000 + 80, 'mapB');
+        let mapC = this.makeActor(-8000 + 80, 8000 + 80, 'mapC');
         let mapD = this.makeActor(8000 + 80, 8000 + 80, 'mapD');
         //setupPC(this.pokitOS);
         let camA = this.makeQuadCam(-8000, -8000); //Top Left Cam
         let camB = this.makeQuadCam(8000, -8000);  //Top Right Cam
         let camC = this.makeQuadCam(-8000, 8000);  //Bottom Left Cam
         let camD = this.makeQuadCam(8000, 8000);   //Bottom Right Cam
-        let camViewA = this.makeQuadCamView(0, 0, camA);
-        let camViewB = this.makeQuadCamView(160, 0, camB);
-        let camViewC = this.makeQuadCamView(0, 160, camC);
-        let camViewD = this.makeQuadCamView(160, 160, camD);
+        let camViewA = this.makeQuadCamView(80, 80, camA);
+        let camViewB = this.makeQuadCamView(240, 80, camB);
+        let camViewC = this.makeQuadCamView(80, 240, camC);
+        let camViewD = this.makeQuadCamView(240, 240, camD);
     }
 
-    makeActor(x, y, texture, z = 0, width, height, scaleX = 1, scaleY = 1) {
+    makeActor(x, y, texture, z = 0, width, height, scaleX = 2, scaleY = 2) {
         return this.ecs.makeEntity({ x: x, y: y, z: z, height: height, width: width, scaleX: scaleX, scaleY: scaleY, },
             ['jewlsTexture', { ID: texture, width: width, height: height, x: 0, y: 0 }],
             ['jewlsActor', {}]);
@@ -52,7 +62,7 @@ export class GameCart {
 
     makeQuadCam(x, y) {
         return this.ecs.makeEntity(
-            [{ x: x, y: y, width: 160, height: 160 }],
+            { x: x, y: y, width: 160, height: 160 },
             ['camera', {}]);
     }
 
