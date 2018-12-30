bf5b6b80739ffb862bad2854e4ac4c6661ec834e
Alex <achythlook@gmail.com>
2018-12-30 13:19:16 -0900

    Bring main site back to life?

---
+++ js/init.mjs
@@ -0,0 +1,20 @@
+import {InputManager} from './smolinput.mjs'
+import {ECS} from './ecs.mjs';
+import Renderer from './smolrender.mjs';
+import PokitOS from './pokitos.mjs';
+import AM from './assetmanager.mjs';
+
+export default function main() {
+    let ecs = new ECS();
+    let e = ecs.makeEntity({width: 320, height: 320});
+    ecs.update();
+    let i = new InputManager();
+    let r = new Renderer(document.querySelector('#gamescreen'));
+    let a = new AM();
+    let pokitOS = new PokitOS({inputmanager: i, ecs: ecs, renderer: r, assets: a});
+    pokitOS.preload();
+    a.getImage('load_text', '/img/bootscreen_text.svg');
+    e.addSystem('img', {imgname:'load_text'})
+    pokitOS.start();
+    return pokitOS;
+}
