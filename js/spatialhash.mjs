25b81349d5b28cb0fce9f04f8734f10030c29005
Alex Chythlook <zaphodious@users-MacBook-Pro.local>
2019-01-01 21:47:11 -0900

    Split more hairs, get better perf

--- js/spatialhash.mjs
+++ js/spatialhash.mjs
@@ -19,39 +19,42 @@ let d = (cs,x,y,z,hw,hh,hd) => Math.ceil((((((x-hw)/cs+(x+hw)/cs))+1)*(((y-hh)/c
 // let newheap = new Int32Array([1,2,3,4,5,6,7,8])
 let fl = Math.floor
 function makeSpatialKey(cs, {x,y,z,width,height,depth}){
-    // keykey = `${cs},${x},${y},${z},${width},${height},${depth}`
-    // memres = keycache.get(keykey)
+    // var keykey = `${cs},${x},${y},${z},${width},${height},${depth}`
+    // var keykey = BigInt(x+y*1000+z*1000000)+BigInt(width*1000000+height*1000+depth)*10000000n
+    // var memres = keycache.get(keykey)
     // if (memres) {
-    //     return memres
+        // return memres
     // }
-    var count = 0;
+    // var count = 0;
     var xi = Math.floor(x/cs)
     var yi = Math.floor(y/cs)
     var zi = Math.floor(z/cs)
     var hw=Math.floor((x+width)/cs)
     var hh=Math.floor((y+height)/cs)
     var hd=Math.floor((z+depth)/cs)
-    var keys = new Int32Array(Math.ceil(((x%cs)/cs)+(width/cs))*Math.ceil(((y%cs)/cs)+(height/cs))*Math.ceil(((z%cs)/cs)+(depth/cs)))
-    console.log('' + xi + ',' + yi + ',' + zi)
-    console.log('' + hw + ',' + hh + ',' + hd)
+    // var keys = new Int32Array(4+Math.ceil(((x%cs)/cs))+Math.ceil(((y%cs)/cs))+Math.ceil(((z%cs)/cs))+Math.floor(width/cs)+Math.floor(height/cs)+Math.floor(depth/cs))
+    var keys = []
+    // console.log('' + xi + ',' + yi + ',' + zi)
+    // console.log('' + hw + ',' + hh + ',' + hd)
     for (xi=Math.floor(x/cs);xi<=hw;xi=xi+1) {
         for (yi=Math.floor(y/cs);yi<=hh;yi=yi+1) {
             for (zi=Math.floor(z/cs);zi<=hd;zi=zi+1) {
                 // keys.push(keypart(xi,yi,zi))
                 // keys.push(`${fl(xi)},${fl(yi)},${fl(zi)}`)
-                keys[count] = ((1e5*xi+1e3*yi+zi)|1)
-                count=count+1;
+                // keys[count] = ((1e5*xi+1e3*yi+zi)|1)
+                keys.push(((1e5*xi+1e3*yi+zi)|1))
+                // count=count+1;
             }
         }
     }
     // keycache.set(keykey,keys)
-    console.log(count)
+    // console.log(count)
     return keys
 }
 function moo() {
     console.time('moo.')
-for (let i = 0;i < 60;i++) {
-    console.log(makeSpatialKey(30,{x:8+i,y:0+i,width:16,height:26,z:1,depth:1}))
+for (let i = 0;i < 600;i++) {
+    makeSpatialKey(30,{x:8+i,y:0+i,width:16,height:26,z:1,depth:1})
 }
     console.timeEnd('moo.')
 }
@@ -60,6 +63,11 @@ for (let i = 0; i < 64e3;i++) {
 
 }
 console.timeEnd('baseline')
+moo()
+moo()
+moo()
+moo()
+moo()
 moo()
 
 //setup: let x=28,y=60,z=5,width=16,height=16,depth=1,cs=30
