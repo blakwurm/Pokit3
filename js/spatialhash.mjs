10bf0382248127fbf19899e5136d311eaa08bafa
Alex Chythlook <zaphodious@users-MacBook-Pro.local>
2019-01-01 22:14:18 -0900

    Clean up source

--- js/spatialhash.mjs
+++ js/spatialhash.mjs
@@ -13,29 +13,8 @@ function makeSpatialKey(cs, {x,y,z,width,height,depth}){
     for (var xi=Math.floor(x/cs);xi<=hw;xi=xi+1) {
         for (var yi=Math.floor(y/cs);yi<=hh;yi=yi+1) {
             for (var zi=Math.floor(z/cs);zi<=hd;zi=zi+1) {
-                keys.push(((1e5*xi+1e3*yi+zi)|1))
+                keys.push(((1e5*xi+1e3*yi+zi)|0))
             }
         }
     }
-    return keys
 }
-function moo() {
-    console.time('moo.')
-for (let i = 0;i < 64e3;i++) {
-    makeSpatialKey(30,{x:8+i,y:0+i,width:16,height:26,z:1,depth:1})
-}
-    console.timeEnd('moo.')
-}
-console.time('baseline')
-for (let i = 0; i < 64e3;i++) {
-
-}
-console.timeEnd('baseline')
-moo()
-moo()
-moo()
-moo()
-moo()
-moo()
-
-//setup: let x=28,y=60,z=5,width=16,height=16,depth=1,cs=30
