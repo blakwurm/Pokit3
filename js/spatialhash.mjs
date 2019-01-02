1c45720960876e3ad22337dbf5e5bf94d918fbf2
Alex Chythlook <zaphodious@users-MacBook-Pro.local>
2019-01-01 21:54:07 -0900

    Get it super speedy

--- js/spatialhash.mjs
+++ js/spatialhash.mjs
@@ -5,55 +5,26 @@ export default class SpatialHash {
     }
     
 }
-let makerange = (min,max) => [...new Array(Math.abs(Math.ceil(max-min))+1).keys()].map(a=>a+Math.ceil(min))
-function* rango(min,max,step=1) {
-    for (let i=min;i<=max;i+=step) {
-        yield i;
-    }
-}
-let keycache = new Map();
-let c = (a, cs) => rango(a[0]/cs, a[1]/cs)
-let d = (cs,x,y,z,hw,hh,hd) => Math.ceil((((((x-hw)/cs+(x+hw)/cs))+1)*(((y-hh)/cs+(y+hh)/cs)+1)*(((z-hd)/cs+(z+hd)/cs)+1)))
-// let d = (cs,x,y,z,hw,hh,hd) => (((x-hw)/cs+(x+hw)/cs)*1e5+((y-hh)/cs+(y+hh)/cs)*1e3+((z-hd)/cs+(z+hd)/cs))
-// cs, x, y, z, width, height, depth
-// let newheap = new Int32Array([1,2,3,4,5,6,7,8])
-let fl = Math.floor
 function makeSpatialKey(cs, {x,y,z,width,height,depth}){
-    // var keykey = `${cs},${x},${y},${z},${width},${height},${depth}`
-    // var keykey = BigInt(x+y*1000+z*1000000)+BigInt(width*1000000+height*1000+depth)*10000000n
-    // var memres = keycache.get(keykey)
-    // if (memres) {
-        // return memres
-    // }
-    // var count = 0;
     var xi = Math.floor(x/cs)
     var yi = Math.floor(y/cs)
     var zi = Math.floor(z/cs)
     var hw=Math.floor((x+width)/cs)
     var hh=Math.floor((y+height)/cs)
     var hd=Math.floor((z+depth)/cs)
-    // var keys = new Int32Array(4+Math.ceil(((x%cs)/cs))+Math.ceil(((y%cs)/cs))+Math.ceil(((z%cs)/cs))+Math.floor(width/cs)+Math.floor(height/cs)+Math.floor(depth/cs))
     var keys = []
-    // console.log('' + xi + ',' + yi + ',' + zi)
-    // console.log('' + hw + ',' + hh + ',' + hd)
     for (xi=Math.floor(x/cs);xi<=hw;xi=xi+1) {
         for (yi=Math.floor(y/cs);yi<=hh;yi=yi+1) {
             for (zi=Math.floor(z/cs);zi<=hd;zi=zi+1) {
-                // keys.push(keypart(xi,yi,zi))
-                // keys.push(`${fl(xi)},${fl(yi)},${fl(zi)}`)
-                // keys[count] = ((1e5*xi+1e3*yi+zi)|1)
                 keys.push(((1e5*xi+1e3*yi+zi)|1))
-                // count=count+1;
             }
         }
     }
-    // keycache.set(keykey,keys)
-    // console.log(count)
     return keys
 }
 function moo() {
     console.time('moo.')
-for (let i = 0;i < 600;i++) {
+for (let i = 0;i < 64e3;i++) {
     makeSpatialKey(30,{x:8+i,y:0+i,width:16,height:26,z:1,depth:1})
 }
     console.timeEnd('moo.')
