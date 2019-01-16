9afdb3c9ac803640b1e9db21a4aae5c4d81a0e79
Croug <officialtoxicdub@gmail.com>
2019-01-15 21:59:07 -0900

    Fix asset manager

--- js/assetmanager.mjs
+++ js/assetmanager.mjs
@@ -10,14 +10,15 @@ export class AssetManager{
         this.assetPipe = new MessageChannel();
     }
     init(pokitOS) {this.pokitOS = pokitOS;}
-    async getImage(imgname, imgurl) {
-        let huh = this.imgs.get(imgname)
-        if (huh) {return huh}
-        let i = new Image();
-        this.imgs.set(imgname, i);
-        i.src = imgurl;
-        console.log(i)
-        return i;
+    async queueImage(id, url) {
+        let i = imgs.get(id);
+        if(i) return id;
+
+        return await new Promise((resolve)=>{
+            let i = new Image();
+            i.onload = ()=>resolve(id);
+            i.src = url;
+        });
     }
     async getJson(jsonName, src) {
         let huh = this.json[jsonName]
