e36728484efdc6b4635f477fef58ca60ec940770
Croug <officialtoxicdub@gmail.com>
2019-01-19 23:27:34 -0900

    fix minor naming errors

--- carts/democart/main.mjs
+++ carts/democart/main.mjs
@@ -1,13 +1,13 @@
 export function main(pokitOS) {
     console.log('loaded')
     let e = pokitOS.ecs.makeEntity({height: 320, width: 320, depth: 10, x: 0, y: 0, z: 10})
-            .addSystem("audioListener", {playOnInit: true, spatial: true, id: 'cali'})
+            .addSystem("audioSource", {startOnInit: true, spatial: true, id: 'cali'})
             .addSystem("img", {id: "offensive"})
             .addSystem('spriteActor')
             .addUniqueSystem('inc', {
                 c: 0,
                 update () {
-                    this.c++;
+                    this.c+=.01;
                     e.x = Math.sin(this.c) * 16 * 20   
                 }
             })
