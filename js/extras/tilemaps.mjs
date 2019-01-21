eeb11d4767124fcc4f16cd953959b56abe92d9a4
Jordan LaPrise <jordanplaprise@gmail.com>
2019-01-21 13:28:46 -0900

    remove unused cog definition

--- js/extras/tilemaps.mjs
+++ js/extras/tilemaps.mjs
@@ -3,7 +3,6 @@ import { Types, AssetManager } from "../assetmanager.mjs";
 export function addTileMapSupport(pokitOS) {
     pokitOS.assets.registerType("TILED") 
     pokitOS.assets.registerDecoder(Types.TILED, decodeTiled)
-    pokitOS.ecs.setCog('tileMap', TileMap);
     // AssetManager.prototype.getTileMap = async function(tilemapName, src) {
     //     let j =  await this.getJson(tilemapName, src)
     // }
